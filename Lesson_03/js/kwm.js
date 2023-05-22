e75eeef90f54cc7a749c34f12f36245a66bb4ab2
jazzmertize <office@neuwersch.eu>
2023-05-22 08:30:32 +0200

    Lesson 03

---
+++ Lesson_03/js/kwm.js
@@ -0,0 +1,37 @@
+"use strict";
+import KWM_Utils from './kwm-utils.js?v=0.2';
+import KWM_Translator from './kwm-translator.js?v=0.2';
+import KWM_Templater from './kwm-templater.js?v=0.2';
+import KWM_Router from './kwm-router.js?v=0.2';
+/**********************************************************************
+ *     Class-Bundle for KWM-Applications.
+ *     App-Shell needs an ID "#kwmJS".
+ *
+ *     @param:
+ *     webRoot - Give me the root-URL of your App
+ *     routes - Give me an Object with "slug" : "template" Routes
+ *     config - Want to store some Config in your App? Here you go!
+ *     ...languages - Give me all languages you want your App to support.
+ *
+ *     KWM - 2022-03-26
+ **********************************************************************/
+
+class kwmJS{
+    constructor(){
+        window.kwm = this;
+        this.options = {
+            appContainer: document.getElementById('kwmJS'),
+            debugMode: true,
+            webRoot: window.location.origin+window.location.pathname,
+        };
+        this.utils = KWM_Utils;
+        this.translator = new KWM_Translator("de", "en");
+        this.templater = new KWM_Templater(this.options.webRoot+"/templates/");
+        this.router = new KWM_Router();
+    }
+
+    t(key){
+        return this.translator.translate(key);
+    }
+}
+new kwmJS();
---
+++ Solution_Lesson_02/templates/shop.tpl
@@ -0,0 +1,3 @@
+<section id="main_content">
+    <h1><%>our_shop<%></h1>
+</section>
